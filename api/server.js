c58826866ef66de1b494a7095fe5f4096202b37e
Sterling Ford <mellowcf@gmail.com>
2021-02-22 23:36:12 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const AcctRouter = require('../accounts/Router')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AcctRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({ API: 'Welcome to the DB'})
+})
 
 module.exports = server;
